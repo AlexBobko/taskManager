d55394c9e6ca080781a3177a787b1afad8a62fb7
AlexBobko <bobko.a.n@gmail.com>
2016-10-26 13:29:22 +0300

    refactor TaskInCheckingCommand

--- web/src/main/java/command/TaskInCheckingCommand.java
+++ web/src/main/java/command/TaskInCheckingCommand.java
@@ -1,17 +1,17 @@
 package command;
 
-import java.text.SimpleDateFormat;
-
 import controller.RequestHandler;
 import dto.Account;
 import dto.TaskDTO;
 import dto.TaskMetaDTO;
-import resources.ConfigurationManager;
-import resources.MessageManager;
+import managers.ConfigurationManager;
+import managers.MessageManager;
+import service.TaskService;
+
+import java.text.SimpleDateFormat;
 
 /** Task set checking status */
 public class TaskInCheckingCommand extends AbsCommand {
-
 	private String page;
 	private StringBuffer message;
 	private boolean b;
@@ -22,12 +22,13 @@ public class TaskInCheckingCommand extends AbsCommand {
 		b = false;
 		try {
 			Account account = (Account) content.getSessionAttributes().get(ACCOUNT);
-			int taskId = (int) Integer.parseInt((String) content.getRequestAttributes().get(CMD_VALUE));
+			int taskId = Integer.parseInt((String) content.getRequestAttributes().get(CMD_VALUE));
 			TaskMetaDTO meta = account.getTasksMeta().get(taskId);
 			TaskDTO task = account.getCurrentTasks().get(taskId);
 			SimpleDateFormat dateFormat = account.getDateFormat();
 			meta.setStatusId(4);// устанавливаем статус на проверке
-			b = updateTaskMeta(meta, task, dateFormat);
+			TaskService taskService=new TaskService();
+			b = taskService.updateTaskMeta(task, meta, dateFormat);
 			if (b) {
 				page = ConfigurationManager.getProperty("path.page.user");
 				message = message.append(MessageManager.getProperty("task.update")).append(meta.getTaskId());
@@ -45,16 +46,4 @@ public class TaskInCheckingCommand extends AbsCommand {
 		content.getSessionAttributes().put(MESSAGE, message.toString());
 		return page;
 	}
-
-	/*
-	 * private boolean updateTaskMeta(TaskMetaDTO meta, TaskDTO task, SimpleDateFormat dateFormat) { TaskMetaDAO metaDao = null; TaskDAO taskDao = null;
-	 * Connection connection = null; try { connection = PoolConnection.getInstance().getConnection(); connection.setAutoCommit(false); metaDao = new
-	 * TaskMetaDAO(connection); b = metaDao.updateStatus(meta); if (b) { Calendar calendar = Calendar.getInstance(); // SimpleDateFormat dateFormat =
-	 * Account.dateFormat; // System.out.println("** AddNewTaskComandImpl.convertDate ** date: " + dateFormat.format(bodyDeadline.getTime())); StringBuffer
-	 * history = task.getHistory(); history.append(dateFormat.format(calendar.getTime())).append("~status:").append(meta.getStatusId()).append("~~"); taskDao =
-	 * new TaskDAO(connection); b = taskDao.updateHistory(task); } if (b) { connection.commit(); } else { connection.rollback(); } } catch (IOException e) {
-	 * e.printStackTrace(); } catch (SQLException e) { e.printStackTrace(); } catch (PropertyVetoException e) { e.printStackTrace(); } finally { try { if
-	 * (connection != null) { connection.setAutoCommit(true); //актуальность удалить и проверить connection.close(); } } catch (SQLException e) {
-	 * e.printStackTrace(); } } return b; }
-	 */
 }
