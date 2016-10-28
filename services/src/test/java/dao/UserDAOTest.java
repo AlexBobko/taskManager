79cc88777daafe3caea12c30627adaca65ccbcd5
AlexBobko <bobko.a.n@gmail.com>
2016-10-28 20:03:55 +0300

    add test

---
+++ services/src/test/java/dao/UserDAOTest.java
@@ -0,0 +1,14 @@
+package dao;
+
+import junit.framework.TestCase;
+
+public class UserDAOTest extends TestCase {
+
+    public void testFindEntityById() throws Exception {
+
+    }
+
+    public void testCreate() throws Exception {
+
+    }
+}
