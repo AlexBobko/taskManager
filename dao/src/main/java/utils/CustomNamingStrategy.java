d57134b1836535231f2fafef0aead332ff18adc4
AlexBobko <bobko.a.n@gmail.com>
2016-11-05 01:53:19 +0300

    hibernate

---
+++ dao/src/main/java/utils/CustomNamingStrategy.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.hibernate.cfg.DefaultNamingStrategy;
+
+public class CustomNamingStrategy extends DefaultNamingStrategy {
+    private String regex = "([A-Z][a-z]+)";
+    private String replacement = "$1_";
+
+    public String classToTableName(String className) {
+        String tableName = className.replaceAll(regex, replacement).toLowerCase();
+        tableName = tableName.substring(0, tableName.length() - 1);
+        return super.classToTableName(tableName);
+    }
+
+    public String propertyToColumnName(String propName) {
+        return super.propertyToColumnName(propName);
+    }
+
+    public String columnName(String columnName) {
+        return columnName;
+    }
+
+    public String tableName(String tableName) {
+        return tableName;
+    }
+}
